1b7d933f7733779f9077228792def3655605ae1a
NikolGraminova <nbackup619@gmail.com>
2023-07-11 12:49:24 +0200

    First project

---
+++ src/org/java/lessons/HelloJava.java
@@ -0,0 +1,10 @@
+package org.java.lessons;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Bye");
+    }
+
+
+}
